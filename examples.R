0dbea1040d003c0ba480d488a583c8e935ded705
Julien Barnier <julien@nozav.org>
2017-05-09 14:57:09 +0200

    Update examples

--- examples.R
+++ examples.R
@@ -6,11 +6,14 @@ res <- wos_search(sid, "TS=computer")
 publis <- wos_retrieve_all(sid, res)
 
 res <- wos_search(sid, "AU=Wickham Hadley")
+publis <- wos_retrieve_all(sid, res)
+publis
+View(publis)
+
 
 res <- wos_search(sid, "AU=Knuth Donald")
 publis <- wos_retrieve_all(sid, res)
-
 publis
-
+View(publis)
 
 
