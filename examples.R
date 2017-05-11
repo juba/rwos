c8d2c4ffc11009f8c9bd3b3613f15bec68afdb7b
Julien Barnier <julien@nozav.org>
2017-05-11 10:18:59 +0200

    Update examples

--- examples.R
+++ examples.R
@@ -1,19 +1,34 @@
-library(rwoslite)
+library(rwos)
 
 sid <- wos_authenticate()
+sid
 
 res <- wos_search(sid, "TS=computer")
-publis <- wos_retrieve_all(sid, res)
+publis <- wos_retrieve_all(res)
 
 res <- wos_search(sid, "AU=Wickham Hadley")
-publis <- wos_retrieve_all(sid, res)
+publis <- wos_retrieve_all(res)
+publis
+View(publis)
+
+publis <- wos_retrieve(res, first = 10, count = 10)
 publis
 View(publis)
 
 
 res <- wos_search(sid, "AU=Knuth Donald")
-publis <- wos_retrieve_all(sid, res)
+publis <- wos_retrieve_all(res)
 publis
 View(publis)
+
+res <- wos_search(sid, "CU=Latvia")
+publis <- wos_retrieve_all(res)
+publis
+View(publis)
+publis <- wos_retrieve(res, first = 11000, count = 800)
+publis <- wos_retrieve(res, first = 10, count = 10)
+
+publis
+
 
 
